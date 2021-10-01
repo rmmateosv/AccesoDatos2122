150786308be539f12a02ad0fc23e3ab1fb787389
Rosa <rmmateosv@gmail.com>
2021-10-01 12:41:04 +0200

    17

--- workspace-eclipse/Unidad1/src/EjerciciosFicheroBinarios/AccesoDatosSocios.java
+++ workspace-eclipse/Unidad1/src/EjerciciosFicheroBinarios/AccesoDatosSocios.java
@@ -1,5 +1,15 @@
 package EjerciciosFicheroBinarios;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class AccesoDatosSocios {
 	private String nombreFichero;
 
@@ -15,6 +25,217 @@ public class AccesoDatosSocios {
 	public void setNombreFichero(String nombreFichero) {
 		this.nombreFichero = nombreFichero;
 	}
+
+	public boolean crearSocio(Socio s) {
+		// TODO Auto-generated method stub
+		boolean resultado = false;
+		
+		//Declaramos el fichero para escribir
+		DataOutputStream fichero = null;		
+		
+		try {
+			//Abrimos el fichero. Si no existe se crea y si existe
+			//añadiremos información
+			fichero = new DataOutputStream(new FileOutputStream(nombreFichero,true));
+			
+			//Escribimos los datos del socio en el fichero
+			//Hacemos que el dni sea de un tamaño fijo de 9
+			StringBuffer dniCon9 = new StringBuffer(s.getDni());
+			dniCon9.setLength(9);
+			fichero.writeChars(dniCon9.toString());
+			
+			fichero.writeChars(s.getNombre()+"\n");
+			fichero.writeLong(s.getFechaN().getTime());
+			fichero.writeBoolean(s.isActivo());
+			
+			resultado = true;
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error, no existe fichero de socios");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
+			if(fichero!=null) {
+				try {
+					fichero.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		return resultado;
+	}
+
+	public ArrayList<Socio> obtenerSocios() {
+		// TODO Auto-generated method stub
+		ArrayList<Socio> resultado = new ArrayList<>();
+		
+		DataInputStream fichero = null;
+		
+		try {
+			fichero = new DataInputStream(new FileInputStream(nombreFichero));
+			
+			//Recorremos el fichero leyendo de forma secuencial hasta que
+			//lleguemos al final de fichero. Cuando se llega al final del
+			//fichero, se produce la excepción EOFException, que hay que capturar
+			//pero no hay que informar.
+			while(true) {
+				
+				Socio s = new Socio();
+				
+				//Leemos el dni. Al ser un String tenemos que leer cada uno
+				//de sus caracteres.
+				String dni="";
+				for(int i=0;i<9;i++) {
+					dni += fichero.readChar();
+				}
+				s.setDni(dni);
+				
+				//Leemos el nombre. Al ser un String de tamaño variable,
+				//hay que leer todos los caracteres hasta encontrar el \n
+				String nombre="";
+				char letra;
+				while((letra=fichero.readChar())!='\n') {
+					nombre+=letra;
+				}
+				s.setNombre(nombre);
+				
+				//Leemos el long de la fecha
+				s.setFechaN(new Date(fichero.readLong()));
+				
+				//Leemos activo
+				s.setActivo(fichero.readBoolean());
+				
+				//Añadimos el socio al resultado
+				resultado.add(s);
+				
+			}
+		} 
+		catch (EOFException e) {
+			// TODO: handle exception
+		}
+		catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Aún no existen socios");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
+			if(fichero!=null) {
+				try {
+					fichero.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		
+		
+		return resultado;
+	}
+
+	public Socio obtenerSocio(String pDni) {
+		// TODO Auto-generated method stub
+		Socio resultado = null;
+		
+		DataInputStream fichero = null;
+		
+		try {
+			fichero = new DataInputStream(new FileInputStream(nombreFichero));
+			
+			//Recorremos el fichero leyendo de forma secuencial hasta que
+			//lleguemos al final de fichero. Cuando se llega al final del
+			//fichero, se produce la excepción EOFException, que hay que capturar
+			//pero no hay que informar.
+			while(true) {				
+				//Leemos el dni. Al ser un String tenemos que leer cada uno
+				//de sus caracteres.
+				String dni="";
+				for(int i=0;i<9;i++) {
+					dni += fichero.readChar();
+				}				
+				
+				if(dni.equalsIgnoreCase(pDni)) {
+					resultado = new Socio();
+					resultado.setDni(dni);
+					//Leemos el nombre. Al ser un String de tamaño variable,
+					//hay que leer todos los caracteres hasta encontrar el \n
+					String nombre="";
+					char letra;
+					while((letra=fichero.readChar())!='\n') {
+						nombre+=letra;
+					}
+					resultado.setNombre(nombre);
+					
+					//Leemos el long de la fecha
+					resultado.setFechaN(new Date(fichero.readLong()));
+					
+					//Leemos activo
+					resultado.setActivo(fichero.readBoolean());
+					
+					return resultado;
+				}
+				else {
+					char letra;
+					String nombre="";
+					while((letra=fichero.readChar())!='\n') {
+						nombre+=letra;
+					}					
+					fichero.readLong();					
+					//Leemos activo
+					fichero.readBoolean();
+				}
+				
+				
+				
+				
+			}
+		} 
+		catch (EOFException e) {
+			// TODO: handle exception
+		}
+		catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Aún no existen socios");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
+			if(fichero!=null) {
+				try {
+					fichero.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		return resultado;
+	}
+
+	public boolean bajaSocio(Socio s) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
--- workspace-eclipse/Unidad1/src/EjerciciosFicheroBinarios/Principal.java
+++ workspace-eclipse/Unidad1/src/EjerciciosFicheroBinarios/Principal.java
@@ -1,7 +1,10 @@
 package EjerciciosFicheroBinarios;
 
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Principal {
@@ -27,7 +30,7 @@ public class Principal {
 
 			switch (opcion) {
 			case 1: {
-				
+				mostrarSocios();
 				break;
 			}
 			case 2: {
@@ -36,6 +39,8 @@ public class Principal {
 				break;
 			}
 			case 3: {
+				
+				bajaSocio();
 				
 				break;
 			}
@@ -58,9 +63,67 @@ public class Principal {
 
 	}
 
+	private static void bajaSocio() {
+		// TODO Auto-generated method stub
+		mostrarSocios();
+		System.out.println("Introduce el dni del socio a modificar");
+		String dni = t.nextLine();
+		
+		Socio s = datos.obtenerSocio(dni);
+		if(s!=null) {
+			if(!datos.bajaSocio(s)) {
+				System.out.println("Error al dar de baja el socio");
+			}
+		}
+		else {
+			System.out.println("No hay socio para ese DNI");
+		}
+	}
+
+	private static void mostrarSocios() {
+		// TODO Auto-generated method stub
+		ArrayList<Socio> socios = datos.obtenerSocios();
+		for(Socio s: socios) {
+			s.mostrar();
+		}
+	}
+
 	private static void crearSocio() {
 		// TODO Auto-generated method stub
 		
+		try {
+			Socio s = new Socio();
+			
+			System.out.println("Introduce DNI");
+			s.setDni(t.nextLine());
+			
+			System.out.println("Introduce Nombre");
+			s.setNombre(t.nextLine());
+			
+			System.out.println("Introduce Fecha de Nacimiento (dd/mm/yyyy)");
+			s.setFechaN(formato.parse(t.nextLine()));
+			
+			//Siempre se crea el socio activo
+			s.setActivo(true);
+			
+			if(!datos.crearSocio(s)) {
+				System.out.println("Error al crear el socio");
+			}
+			
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error en la fecha");
+		}
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 	}
