25b9540b873d833c6cef8f990ddf31eafb63d9f6
Rosa <rmmateosv@gmail.com>
2021-10-04 09:47:58 +0200

    18

--- workspace-eclipse/Unidad1/src/EjerciciosFicheroBinarios/AccesoDatosSocios.java
+++ workspace-eclipse/Unidad1/src/EjerciciosFicheroBinarios/AccesoDatosSocios.java
@@ -3,6 +3,7 @@ package EjerciciosFicheroBinarios;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -151,7 +152,8 @@ public class AccesoDatosSocios {
 			fichero = new DataInputStream(new FileInputStream(nombreFichero));
 			
 			//Recorremos el fichero leyendo de forma secuencial hasta que
-			//lleguemos al final de fichero. Cuando se llega al final del
+			//lleguemos al final de fichero o se encuentre el socio buscado. 
+			// Cuando se llega al final del
 			//fichero, se produce la excepción EOFException, que hay que capturar
 			//pero no hay que informar.
 			while(true) {				
@@ -162,7 +164,7 @@ public class AccesoDatosSocios {
 					dni += fichero.readChar();
 				}				
 				
-				if(dni.equalsIgnoreCase(pDni)) {
+				if(dni.trim().equalsIgnoreCase(pDni)) {
 					resultado = new Socio();
 					resultado.setDni(dni);
 					//Leemos el nombre. Al ser un String de tamaño variable,
@@ -183,10 +185,13 @@ public class AccesoDatosSocios {
 					return resultado;
 				}
 				else {
+					//Leo el resto de datos del registro actual
+					//para dejar el puntero del fichero
+					//bien posicionado y poder leer el siguiente dni
 					char letra;
-					String nombre="";
+					
 					while((letra=fichero.readChar())!='\n') {
-						nombre+=letra;
+						
 					}					
 					fichero.readLong();					
 					//Leemos activo
@@ -224,7 +229,94 @@ public class AccesoDatosSocios {
 
 	public boolean bajaSocio(Socio s) {
 		// TODO Auto-generated method stub
-		return false;
+		boolean resultado=false;
+		
+		//Declaramos fichero original y temporal
+		DataInputStream fOriginal = null;
+		DataOutputStream fTemporal = null;
+		
+		try {
+			fOriginal = new DataInputStream(new FileInputStream(nombreFichero));
+			fTemporal = new DataOutputStream(new FileOutputStream("socios.tmp",false));
+			
+			//Recorremos socios.bin
+			while(true) {
+				//Leemos datos de socio
+				Socio registro = new Socio();
+				
+				String dni="";
+				for(int i=0;i<9;i++) {
+					dni += fOriginal.readChar();
+				}				
+				registro.setDni(dni);
+				//Leemos el nombre. Al ser un String de tamaño variable,
+				//hay que leer todos los caracteres hasta encontrar el \n
+				String nombre="";
+				char letra;
+				while((letra=fOriginal.readChar())!='\n') {
+					nombre+=letra;
+				}
+				registro.setNombre(nombre);
+				
+				//Leemos el long de la fecha
+				registro.setFechaN(new Date(fOriginal.readLong()));
+				
+				//Leemos activo
+				registro.setActivo(fOriginal.readBoolean());
+				
+				//Comprobamos si es el socio a dar de baja
+				if(s.getDni().equalsIgnoreCase(registro.getDni())) {
+					registro.setActivo(false);
+				}
+				//Escribimos los datos en el fichero temporal
+				fTemporal.writeChars(registro.getDni());
+				fTemporal.writeChars(registro.getNombre()+"\n");
+				fTemporal.writeLong(registro.getFechaN().getTime());
+				fTemporal.writeBoolean(registro.isActivo());
+			}
+		} 
+		
+		catch (EOFException e) {
+			// TODO: handle exception
+		}
+		catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if(fOriginal!=null)
+					fOriginal.close();
+				
+				if(fTemporal!=null)
+					fTemporal.close();
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		//Borrar fichero socios.bin
+		File fO = new File(nombreFichero);
+		if(fO.delete()) {
+			File fTmp = new File("socios.tmp");
+			if(!fTmp.renameTo(fO)) {
+				System.out.println("Error al renombrar fichero temporal");
+			}
+			else {
+				resultado = true;
+			}
+			
+		}
+		else {
+			System.out.println("Error al borrar socios.bin");
+		}
+		
+		return resultado;
 	}
 	
 	
