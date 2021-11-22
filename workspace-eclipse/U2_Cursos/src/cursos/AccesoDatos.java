92c8f3c052603e0c4af9b140932211d946d8354e
Rosa <rmmateosv@gmail.com>
2021-11-22 10:23:17 +0100

    49

--- workspace-eclipse/U2_Cursos/src/cursos/AccesoDatos.java
+++ workspace-eclipse/U2_Cursos/src/cursos/AccesoDatos.java
@@ -1,5 +1,133 @@
 package cursos;
 
-public class AccesoDatos {
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
+import java.util.ArrayList;
 
+public class AccesoDatos {
+	private Connection conexion = null;
+	private String usuario = "root",
+			clave="root",
+			url="jdbc:mysql://localhost:3306/cursos";
+	
+	public AccesoDatos() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			conexion = DriverManager.getConnection(url, usuario, clave);
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public Connection getConexion() {
+		return conexion;
+	}
+
+	public void setConexion(Connection conexion) {
+		this.conexion = conexion;
+	}
+	
+	public void cerrar() {
+		try {
+			conexion.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public String login(String pUs, String pClave) {
+		// TODO Auto-generated method stub
+		String resultado = null;
+		
+		try {
+			CallableStatement funcion = conexion.prepareCall(
+					"{? = call login(?,?)}");
+			funcion.setString(2, pUs);
+			funcion.setString(3, pClave);
+			//Registrar el tipo del parámetro de salida
+			funcion.registerOutParameter(1, Types.VARCHAR);
+			funcion.executeUpdate();
+			//Recuperamos el valor devuelto por la función
+			resultado = funcion.getString(1);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return resultado;
+	}
+
+	public boolean existeAlumno(String pUs) {
+		// TODO Auto-generated method stub
+		boolean resultado = false;
+		try {
+			PreparedStatement sentencia = 
+					conexion.prepareStatement("select * "
+							+ "from usuario where usuario = ?");
+			sentencia.setString(1, pUs);
+			ResultSet r = sentencia.executeQuery();
+			if(r.next()) {
+				resultado = true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return resultado;
+	}
+
+	public boolean crearAlumno(String pUs) {
+		// TODO Auto-generated method stub
+		boolean resultado= false;
+		try {
+			PreparedStatement sentencia = 
+					conexion.prepareStatement("insert into usuario "
+							+ "values(?,sha2(?,0),'Alumno')");
+			sentencia.setString(1, pUs);
+			sentencia.setString(2, pUs);
+			
+			int filas = sentencia.executeUpdate();
+			if(filas == 1) {
+				resultado = true;
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return resultado;
+	}
+
+	public ArrayList<String> obtenerAlumnos() {
+		// TODO Auto-generated method stub
+		ArrayList<String> resultado = new ArrayList<>();
+		 
+		try {
+			Statement sentencia = conexion.createStatement();
+			ResultSet r = sentencia.executeQuery("select * from usuario "
+					+ "where perfil = 'Alumno'");
+			while(r.next()) {
+				resultado.add(r.getString(1));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return resultado;
+	}
+	
 }
--- workspace-eclipse/U2_Cursos/src/cursos/Principal.java
+++ workspace-eclipse/U2_Cursos/src/cursos/Principal.java
@@ -1,10 +1,164 @@
 package cursos;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes.Array;
+
 public class Principal {
+
+	static Scanner t = new java.util.Scanner(System.in);
+	static AccesoDatos ad = new AccesoDatos();
+	static SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yy");
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		if (ad.getConexion() != null) {
+			login();
+			ad.cerrar();
+		} else {
+			System.out.println("Error, no se ha podido conectar con la BD");
+		}
+	}
 
+	private static void login() {
+		// TODO Auto-generated method stub
+		System.out.println("Usuario");
+		String usuario = t.nextLine();
+		System.out.println("Contraseña:");
+		String clave = t.nextLine();
+		
+		String perfil = ad.login(usuario,clave);
+		if(perfil== null || perfil.equalsIgnoreCase("NE")){
+			System.out.println("Error, usuario o contraseña incorrecto");
+		}
+		else {
+			if(perfil.equalsIgnoreCase("Admin")) {
+				menuAdmin();
+			}
+			else {
+				menuUsuario();
+			}
+		}
+	}
+
+	private static void menuUsuario() {
+		// TODO Auto-generated method stub
+		System.out.println("Menú Alumno");
+		int opcion = 0;
+		do {
+			System.out.println("Introduce una opcíon:");
+			System.out.println("0-Salir");
+			System.out.println("1-Modificar contraseña");
+			System.out.println("2-Mostrar Cursos (con patrón)");
+			System.out.println("3-Matricularse en un curso");
+			System.out.println("4-Anular matrícula");
+			System.out.println("5-Ver resultados curso");
+			System.out.println("6-Estadística");
+			
+			opcion = t.nextInt();t.nextLine();
+			
+			switch (opcion) {
+				case 1: {
+					
+					break;
+				}
+				case 2: {
+					
+					break;
+				}
+				case 3: {
+					
+					break;
+				}
+				case 4: {
+					
+					break;
+				}
+				case 5: {
+					
+					break;
+				}
+				case 6: {
+					
+					break;
+				}
+				
+			}
+			
+		} while (opcion != 0);
+	}
+
+	private static void menuAdmin() {
+		// TODO Auto-generated method stub
+		System.out.println("Menú Administrador");
+		int opcion = 0;
+		do {
+			System.out.println("Introduce una opcíon:");
+			System.out.println("0-Salir");
+			System.out.println("1-Crear Alumno");
+			System.out.println("2-Mostrar Alumnos (con patrón)");
+			System.out.println("3-Borrar Alumno");
+			System.out.println("4-Crear Curso");
+			System.out.println("5-Mostrar Cursos");
+			System.out.println("6-Modificar Curso");
+			
+			opcion = t.nextInt();t.nextLine();
+			
+			switch (opcion) {
+				case 1: {
+					crearAlumno();
+					break;
+				}
+				case 2: {
+					mostrarAlumnos();
+					break;
+				}
+				case 3: {
+					
+					break;
+				}
+				case 4: {
+					
+					break;
+				}
+				case 5: {
+					
+					break;
+				}
+				case 6: {
+					
+					break;
+				}
+				
+			}
+			
+		} while (opcion != 0);
+	}
+
+	private static void mostrarAlumnos() {
+		// TODO Auto-generated method stub
+		ArrayList<String> alumnos = ad.obtenerAlumnos();
+		for(String nombre:alumnos) {
+			System.out.println("Alumno:" + nombre);
+		}
+	}
+
+	private static void crearAlumno() {
+		// TODO Auto-generated method stub
+		System.out.println("Nombre Usuario a crear:");
+		String usuario = t.nextLine();
+		//Chequear si hay otro usuario con el mismo nombre
+		if(!ad.existeAlumno(usuario)) {
+			if(!ad.crearAlumno(usuario)) {
+				System.out.println("Error al dar de alta el alumno");
+			}
+		}
+		else {
+			System.out.println("Error, ya hay un alumno con ese nombre");
+		}
 	}
 
 }
