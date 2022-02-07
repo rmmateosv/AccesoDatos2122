d1e5d289ea72799910a31891f87224ebbfa566ca
Rosa <rmmateosv@gmail.com>
2022-02-07 09:10:58 +0100

    71

--- workspace-eclipse/U4_ACB/src/acb/AccesoDatos.java
+++ workspace-eclipse/U4_ACB/src/acb/AccesoDatos.java
@@ -1,5 +1,40 @@
 package acb;
 
-public class AccesoDatos {
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
 
+public class AccesoDatos {
+	EntityManager conexion = null;
+
+	public AccesoDatos() {
+		try {
+			
+			conexion = 
+					Persistence.
+					createEntityManagerFactory("UP_ACB").
+					createEntityManager();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+
+	public void cerrar() {
+		try {
+			conexion.close();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	public EntityManager getConexion() {
+		return conexion;
+	
+	}
+
+	public void setConexion(EntityManager conexion) {
+		this.conexion = conexion;
+	}
 }
--- workspace-eclipse/U4_ACB/src/acb/Principal.java
+++ workspace-eclipse/U4_ACB/src/acb/Principal.java
@@ -1,14 +1,52 @@
 package acb;
 
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 
 public class Principal {
+	static Scanner t = new java.util.Scanner(System.in);
+	static AccesoDatos ad = new AccesoDatos();
+	static SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yy");
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		EntityManager conexion = 
-	Persistence.createEntityManagerFactory("UP_ACB").createEntityManager();
+		if (ad.getConexion() != null) {
+			int opcion = 0;
+			do {
+				System.out.println("Introduce una opcíon:");
+				System.out.println("0-Salir");
+				System.out.println("1-Seleccionar Partido");
+				System.out.println("2-Registra Acción");
+				System.out.println("3-Anular Acción");
+				System.out.println("4-Borrar Partido");
+				System.out.println("5-Estadística Partido");
+				switch (opcion) {
+					case 1: {
+	
+						break;
+					}
+					case 2: {
+						
+						break;
+					}
+					case 3: {
+						
+						break;
+					}
+					case 4: {
+						
+						break;
+					}
+					case 5: {
+						
+						break;
+					}
+				}
+			} while (opcion != 0);
+			ad.cerrar();
+		}
 	}
-
 }
